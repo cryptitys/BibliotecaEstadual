da1b0e5af2c43a67c2c4061b622a6e39d2234fd3
Vtr.bwr <vrodriguespereira11@gmail.com>
2025-06-29 14:14:29 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
   authDomain: "SUA_AUTH_DOMAIN",
   projectId: "biblioteca-escola-27dd7",
   storageBucket: "SUA_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  messagingSenderId: "SUA_MESSAGING_SENDER_ID",
   appId: "SUA_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
